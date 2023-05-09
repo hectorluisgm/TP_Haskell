2bfc7176df677ddfca40fc6b1fab851c44604bcf
Hector Gomez <hectorluisgomezmoya@gmail.com>
2023-05-09 20:14:45 -0300

    Agregado del Ejercicio 1

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -37,7 +37,7 @@ likesDePublicacion (_, _, us) = us
 -- Ejercicios
 
 nombresDeUsuarios :: RedSocial -> [String]
-nombresDeUsuarios = undefined
+nombresDeUsuarios redSocial| redSocialValida redSocial ==True =proyectarNombres(usuarios(redSocial))
 
 noHayIdsRepetidos :: [Usuario] -> Bool
 noHayIdsRepetidos [] = True
@@ -64,6 +64,100 @@ usuariosValidos :: [Usuario] -> Bool
 usuariosValidos [] = True
 usuariosValidos (x:xs) | noHayIdsRepetidos (x:xs) == True && usuarioValido x == True = usuariosValidos (xs)
                        | otherwise = False
+
+hacerListaRelacion:: [Relacion] -> [(Integer, Integer)]
+hacerListaRelacion [] = []
+hacerListaRelacion (x:xs) = ((idDeUsuario(fst (x)), (idDeUsuario(snd (x)))): hacerListaRelacion(xs))
+
+
+perteneceUsuario :: Usuario -> [Usuario] -> Bool
+perteneceUsuario a [] = False
+perteneceUsuario a (x:xs) | a == x = True 
+                          | otherwise = perteneceUsuario a xs
+
+perteneceRelacion :: Relacion -> [Relacion] -> Bool
+perteneceRelacion a [] = False
+perteneceRelacion a (x:xs) | a == x = True 
+                           | otherwise = perteneceRelacion a xs
+
+pertenece2 :: (Integer, Integer) -> [(Integer,Integer)] -> Bool
+pertenece2 a [] = False
+pertenece2 a (x:xs) | a == x = True 
+                   | otherwise = pertenece2 a xs
+                   
+relacionesValidas :: [Usuario] -> [Relacion] -> Bool
+relacionesValidas [] (y:ys) = False
+relacionesValidas (x:xs) [] = True
+relacionesValidas (x:xs) (y:ys)| usuariosDeRelacionesValidos (x:xs) (y:ys) == True && relacionesAsimetricas (y:ys) == True && noHayRelacionesRepetidas (y:ys) == True = True
+                               |otherwise = False
+                               
+usuariosDeRelacionesValidos :: [Usuario] -> [Relacion] -> Bool
+usuariosDeRelacionesValidos (x:xs) [] = True
+usuariosDeRelacionesValidos [] (y:ys) = False
+usuariosDeRelacionesValidos (x:xs) (y:ys) | fst(y) == snd(y) || perteneceUsuario (fst(y)) ((x:xs)) == False || perteneceUsuario (snd(y)) ((x:xs)) == False = False
+                                          | otherwise = usuariosDeRelacionesValidos (x:xs) ys
+
+relacionesAsimetricas :: [Relacion] -> Bool
+relacionesAsimetricas [] = True
+relacionesAsimetricas (x:xs) | perteneceRelacion ((snd (x), fst (x))) (x:xs) == True = False
+                             |otherwise = relacionesAsimetricas xs
+
+noHayRelacionesRepetidas :: [Relacion] -> Bool
+noHayRelacionesRepetidas [] = True
+noHayRelacionesRepetidas (x:xs) | pertenece2 (idDeUsuario (fst (x)), idDeUsuario(snd(x))) (hacerListaRelacion xs) == True =False
+                                | otherwise = noHayRelacionesRepetidas (xs)
+
+usuariosDePublicacionSonUsuariosDeRed :: [Usuario] -> [Publicacion] -> Bool
+usuariosDePublicacionSonUsuariosDeRed [] (y:ys) = False
+usuariosDePublicacionSonUsuariosDeRed (x:xs) [] = True
+usuariosDePublicacionSonUsuariosDeRed (x:xs) ((usuario, _, _):ys) | perteneceUsuario (usuario) (x:xs) == False = False
+                                                    | otherwise = usuariosDePublicacionSonUsuariosDeRed (x:xs) ys
+
+usuariosDeLikeDePublicacionSonUsuariosDeRed :: [Usuario] -> [Publicacion] -> Bool
+usuariosDeLikeDePublicacionSonUsuariosDeRed [] (y:ys) = False
+usuariosDeLikeDePublicacionSonUsuariosDeRed (x:xs) [] = True
+usuariosDeLikeDePublicacionSonUsuariosDeRed (x:xs) ((_,_,usuarios):ys) | usuariosLikeValidos (x:xs) (usuarios) == False = False
+                                                                     | otherwise = usuariosDeLikeDePublicacionSonUsuariosDeRed (x:xs) ys
+
+usuariosLikeValidos :: [Usuario] -> [Usuario] -> Bool
+usuariosLikeValidos [] (y:ys) = False
+usuariosLikeValidos (x:xs) [] = True
+usuariosLikeValidos (x:xs) (y:ys) | perteneceUsuario y (x:xs) == False = False
+                                  | otherwise = usuariosLikeValidos ys (x:xs)
+
+hacerListaUsuariosPublicaciones:: [Publicacion] -> [Integer]
+hacerListaUsuariosPublicaciones [] = []
+hacerListaUsuariosPublicaciones ((us, _, _):xs) = (idDeUsuario(us): hacerListaUsuariosPublicaciones(xs))
+
+hacerListaPublicaciones:: [Publicacion] -> [String]
+hacerListaPublicaciones [] = []
+hacerListaPublicaciones ((_, publicacion, _):xs) = ((publicacion): hacerListaPublicaciones(xs))
+
+perteneceString :: String -> [String] -> Bool
+perteneceString a [] = False
+perteneceString a (x:xs) | a == x = True 
+                   | otherwise = perteneceString a xs
+
+noHayPublicacionesRepetidas :: [Publicacion] -> Bool
+noHayPublicacionesRepetidas [] = True
+noHayPublicacionesRepetidas ((usuario,publicacion,_):xs)| pertenece (idDeUsuario(usuario)) (hacerListaUsuariosPublicaciones(xs)) == True || perteneceString (publicacion) (hacerListaPublicaciones(xs)) ==True =False
+                                                        |otherwise = noHayPublicacionesRepetidas (xs) 
+
+publicacionesValidas :: [Usuario] -> [Publicacion] -> Bool
+publicacionesValidas (x:xs) [] = True
+publicacionesValidas [] (y:ys) = False
+publicacionesValidas (x:xs) (y:ys) | usuariosDePublicacionSonUsuariosDeRed (x:xs) (y:ys)==True && usuariosDeLikeDePublicacionSonUsuariosDeRed (x:xs) (y:ys)==True && noHayPublicacionesRepetidas (y:ys)==True = True
+                                   |otherwise = False
+
+redSocialValida :: RedSocial -> Bool
+redSocialValida ([], _, _) = False
+redSocialValida (_, [], _) = True
+redSocialValida (_, _, []) = True
+redSocialValida (usuarios, relaciones, publicaciones) | usuariosValidos usuarios ==True && relacionesValidas (usuarios) (relaciones) ==True && publicacionesValidas (usuarios) (publicaciones) ==True = True
+                                                      | otherwise = False
+proyectarNombres :: [Usuario] -> [String]
+proyectarNombres [] = []
+proyectarNombres (x:xs) =(snd(x):proyectarNombres(xs))
 
 -- describir qué hace la función: .....
 amigosDe :: RedSocial -> Usuario -> [Usuario]
